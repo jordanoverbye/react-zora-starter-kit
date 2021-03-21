7fbb31ab192ae65e776d060cc4264becd0b9b0f0
Jordan Overbye <jordanoverbye@gmail.com>
2021-03-21 18:48:14 +1000

    Render mint form

--- pages/mint.js
+++ pages/mint.js
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import Head from 'next/head';
 
 import { Container, Header, Footer } from '../components/Layout';
+import { MintForm } from '../components/MintForm';
 
 export default function Mint() {
   return (
@@ -16,6 +17,9 @@ export default function Mint() {
           <section className="py-24 text-center space-y-8">
             <h1 className="text-8xl font-bold">Mint</h1>
             <p className="text-2xl text-gray-400 max-w-2xl mx-auto">Use the form below to mint a zOrb</p>
+          </section>
+          <section className="max-w-3xl mx-auto">
+            <MintForm />
           </section>
         </main>
         <Footer />
